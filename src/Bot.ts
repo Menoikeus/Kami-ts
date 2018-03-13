babf0cc1902dcf0fe3fb0aab2c99df71d5ee4c84
Dat-Thanh Nguyen <datonnguyen@gmail.com>
2018-03-13 04:14:09 -0400

    Started mongodb connection and the execution of commands

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,15 +1,20 @@
-import { CommandoClient } from 'discord.js-commando';
+import { Client, Message } from 'discord.js';
+import { CommandHandler } from './services/CommandService';
+import { LeagueCommands } from './commands/inhouse/league/LeagueCommands';
+import { ProfileCommands } from './commands/inhouse/profile/ProfileCommands';
+import { MongoDatabaseProvider } from './services/MongoDBService';
+import { MongoClient } from 'mongodb';
 const config = require('./config/global_config.json');
+const mongodb_config = require('./config/mongodb/mongo_config.json');
 const path = require('path');
 
 export class Bot {
-  private client: CommandoClient;
+  private client: Client;
+  private commandHandler;
 
-  constructor(ownerId: string, token: string) {
-    this.client = new CommandoClient({
-      owner: ownerId,
-      commandPrefix: config.defaultPrefix
-    });
+  constructor(token: string) {
+    this.client = new Client();
+    this.commandHandler = new CommandHandler(this.client);
 
     this.client.on('ready', () => {
       console.log("I am ready!");
@@ -17,16 +22,31 @@ export class Bot {
     });
 
     this.client.on('error', console.error);
-
-    this.client.registry.registerGroups([
-        ['inhouse', 'Inhouse commands'],
-        ['general', 'General commands']
-      ]);
-
-    this.client.registry.registerDefaults();
-
-    this.client.registry.registerCommandsIn(path.join(__dirname, 'commands/inhouse'));
-
     this.client.login(token);
+
+    this.setupDatabase();
+    setTimeout(this.setupCommands.bind(this), 1000);
+  }
+
+  private setupDatabase(): void {
+    new MongoDatabaseProvider(mongodb_config.user, mongodb_config.password, mongodb_config.url);
+  }
+
+  private setupCommands(): void {
+    this.commandHandler.addCommands(new LeagueCommands());
+    this.commandHandler.addCommands(new ProfileCommands());
+
+    console.log("Commands added!");
+    let db: MongoClient = MongoDatabaseProvider.getDatabase();
+    this.client.on('message', async (message: Message) => {
+      if(message.author.bot) return;
+
+      const directoryInfo = await db.db(message.guild.id).collection("info").findOne({ info_type: "directory_info" });
+      const server_prefix: string = directoryInfo.s_prefix;
+
+      if(!message.content.startsWith(server_prefix)) return;
+
+      this.commandHandler.findAndRun(message, server_prefix);
+    });
   }
 }
--- src/services/MongoDBService.ts
+++ src/services/MongoDBService.ts
@@ -1,14 +1,24 @@
 import { MongoClient, Collection } from "mongodb";
 
 export class MongoDatabaseProvider {
-  private static db: MongoClient;
+  static db: MongoClient;
 
   constructor(user: string, password: string, databaseUrl: string) {
     const uri = "mongodb://" + user + ":" + password + "@" + databaseUrl;
 
     MongoClient.connect(uri, function(error, client) {
+      if(error) {
+        throw new Error("Database connection failed");
+      }
       MongoDatabaseProvider.db = client;
       console.log("Database connection established!");
     });
+  }
+
+  public static getDatabase(): MongoClient {
+    if(this.db === undefined) {
+      throw new Error("No connection has been established with any database yet!");
+    }
+    return this.db;
   }
 }
